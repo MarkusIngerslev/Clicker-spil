7af68c6791a9efa9bc39457f19bb4bfa2b79802b
MarkusIngerslev <markusolsen2004@gmail.com>
2023-02-21 15:49:36 +0100

    animation restart når klikket på enten hund eller and

--- game.js
+++ game.js
@@ -29,27 +29,84 @@ function duckClick() {
     .querySelector("#duck1_container")
     .removeEventListener("click", duckClick);
   document.querySelector("#duck1_container").classList.add("paused");
-  document.querySelector("#duck1_sprite").classList.add("goodClick");
+  document.querySelector("#duck1_sprite").classList.add("zoom_out");
 
+  // Tilføjer nummer til variablen points
   console.log("plusNumber");
   points++;
   displayNumber();
+
+  //når forsvind animationen er færdig, duckGone
+
+  document
+    .querySelector("#duck1_container")
+    .addEventListener("animationend", duckGone);
 }
 
-function normalDuck() {
-  console.log("normalDuck");
-  document.querySelector("#duck1_container").removeEventListener("paused");
-  document.querySelector("#duck1_sprite").removeEventListener("goodclick");
+function duckGone() {
+  //Fjerner evnet der startede functionen
+  document
+    .querySelector("#duck1_container")
+    .removeEventListener("animationend", duckGone);
+
+  //Fjerner class med forsvind animation
+  document.querySelector("#duck1_sprite").classList.remove("zoom_out");
+
+  //Fjerner pause fra container
+  document.querySelector("#duck1_container").classList.remove("paused");
+
+  //Genstarter bevægelse fra venstre mod højre
+  document.querySelector("#duck1_container").classList.remove("duck_move");
+  document.querySelector("#duck1_container").offsetWidth;
+  document.querySelector("#duck1_container").classList.add("duck_move");
+
+  // Tilføjer event så anden kan klikkes på igen
+  document
+    .querySelector("#duck1_container")
+    .addEventListener("click", duckClick);
 }
 
 // Clicking on a bad element
 function dogClick() {
   console.log("dogClick");
+
+  //Fjerner event så hun ikke kan klikkes på igen
   document
     .querySelector("#dog1_container")
     .removeEventListener("click", dogClick);
+
+  // pauser move animationen for hund-container
   document.querySelector("#dog1_container").classList.add("paused");
-  document.querySelector("#dog1_sprite").classList.add("goodClick");
+  // laver zoom_out animation på selve spriten af hunden
+  document.querySelector("#dog1_sprite").classList.add("zoom_in");
+
+  //erstater classen active heart med broken heart i scoreboard
   document.querySelector("#heart1_sprite").classList.remove("active_heart");
   document.querySelector("#heart1_sprite").classList.add("broken_heart");
+
+  //når forsvind animationen er færdig så kør dogGone event
+  document
+    .querySelector("#dog1_container")
+    .addEventListener("animationend", dogGone);
+}
+
+function dogGone() {
+  //Fjerner evnet der startede functionen
+  document
+    .querySelector("#dog1_container")
+    .removeEventListener("animationend", dogGone);
+
+  //Fjerner class med forsvind animation
+  document.querySelector("#dog1_sprite").classList.remove("zoom_in");
+
+  //Fjerner pause fra container
+  document.querySelector("#dog1_container").classList.remove("paused");
+
+  //Genstarter bevægelse fra venstre mod højre
+  document.querySelector("#dog1_container").classList.remove("dog_move");
+  document.querySelector("#dog1_container").offsetWidth;
+  document.querySelector("#dog1_container").classList.add("dog_move");
+
+  // Tilføjer event så anden kan klikkes på igen
+  document.querySelector("#dog1_container").addEventListener("click", dogClick);
 }
