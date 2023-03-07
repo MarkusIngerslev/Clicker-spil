cee994fb95b6ee0e3810cceb75a7da0bd6f271cb
MarkusIngerslev <markusolsen2004@gmail.com>
2023-03-07 23:30:43 +0100

    fikset så elementer ikke spawner midt i ingen ting
    Jeg har fikset at de forskellige dyr ikke spawner midt på skærmen. Jeg har gjort dette ved at dele restart elementet op i 3 forskellige funtioner, som der nu har fikset problemet.

--- game.js
+++ game.js
@@ -7,11 +7,13 @@ let lives = 3;
 
 function ready() {
   console.log("JavaScript is ready!");
-  document.querySelector("#btn_start").addEventListener("click", startGame);
-  document.querySelector("#btn_restart").addEventListener("click", startGame);
+  document.querySelector("#btn_start").addEventListener("mousedown", startGame);
+  document
+    .querySelector("#btn_restart")
+    .addEventListener("mousedown", startGame);
   document
     .querySelector("#btn_go_to_start")
-    .addEventListener("click", showStartScreen);
+    .addEventListener("mousedown", showStartScreen);
 }
 
 function startGame() {
@@ -28,7 +30,7 @@ function startGame() {
 
   // skjuler start skærmen efter spillet starter
   document.querySelector("#start").classList.add("hidden");
-
+  document.querySelector("#game_elements").classList.remove("hidden");
   // Til føjer bevægelse til hver container
   startAnimationer();
 
@@ -38,7 +40,7 @@ function startGame() {
   // Tilføjer start positioner til containerne
   startPositioner();
 
-  // Tilføjer click event til hver container
+  // Tilføjer mousedown event til hver container
   startClick();
 
   // Tilføjer event så containerne har en ny position når der er kørt over skærmen.
@@ -50,6 +52,8 @@ function startGame() {
 // ========================== \\
 
 function startAnimationer() {
+  // kalder funtion for at fjerne paused fra container
+  dyrPlay();
   document.querySelector("#duck1_container").classList.add("duck_move1");
   document.querySelector("#duck2_container").classList.add("duck_move2");
   document.querySelector("#duck3_container").classList.add("duck_move3");
@@ -80,22 +84,26 @@ function startPositioner() {
 function startClick() {
   document
     .querySelector("#duck1_container")
-    .addEventListener("click", duckClick);
+    .addEventListener("mousedown", duckClick);
   document
     .querySelector("#duck2_container")
-    .addEventListener("click", duckClick);
+    .addEventListener("mousedown", duckClick);
   document
     .querySelector("#duck3_container")
-    .addEventListener("click", duckClick);
-  document.querySelector("#dog1_container").addEventListener("click", dogClick);
-  document.querySelector("#dog2_container").addEventListener("click", dogClick);
+    .addEventListener("mousedown", duckClick);
+  document
+    .querySelector("#dog1_container")
+    .addEventListener("mousedown", dogClick);
+  document
+    .querySelector("#dog2_container")
+    .addEventListener("mousedown", dogClick);
 
   document
     .querySelector("#goose1_container")
-    .addEventListener("click", gooseClick);
+    .addEventListener("mousedown", gooseClick);
   document
     .querySelector("#goose2_container")
-    .addEventListener("click", gooseClick);
+    .addEventListener("mousedown", gooseClick);
 }
 
 function positionRestart() {
@@ -246,30 +254,33 @@ function stopGame() {
   document.querySelector("#sound_hunting").pause();
   document.querySelector("#sound_dogshot").pause();
   document.querySelector("#sound_hunting").currentTime = 0;
-  // fjerner click
+  // fjerner mousedown
   document
     .querySelector("#duck1_container")
-    .removeEventListener("click", duckClick);
+    .removeEventListener("mousedown", duckClick);
   document
     .querySelector("#duck2_container")
-    .removeEventListener("click", duckClick);
+    .removeEventListener("mousedown", duckClick);
   document
     .querySelector("#duck3_container")
-    .removeEventListener("click", duckClick);
+    .removeEventListener("mousedown", duckClick);
   document
     .querySelector("#dog1_container")
-    .removeEventListener("click", dogClick);
+    .removeEventListener("mousedown", dogClick);
   document
     .querySelector("#dog2_container")
-    .removeEventListener("click", dogClick);
+    .removeEventListener("mousedown", dogClick);
   document
     .querySelector("#goose1_container")
-    .removeEventListener("click", gooseClick);
+    .removeEventListener("mousedown", gooseClick);
   document
     .querySelector("#goose2_container")
-    .removeEventListener("click", gooseClick);
+    .removeEventListener("mousedown", gooseClick);
 
   // Stopper animation
+
+  dyrPaused();
+
   document
     .querySelector("#duck1_container")
     .classList.remove("duck_move1", "duck_move2", "duck_move3");
@@ -296,6 +307,28 @@ function stopGame() {
   document.querySelector("#time_sprite").classList.remove("shrink");
 }
 
+// Funtion til at pause alle game elementer
+function dyrPaused() {
+  console.log("DyrPaused");
+  document.querySelector("#duck1_container").classList.add("paused");
+  document.querySelector("#duck2_container").classList.add("paused");
+  document.querySelector("#duck3_container").classList.add("paused");
+  document.querySelector("#goose1_container").classList.add("paused");
+  document.querySelector("#goose1_container").classList.add("paused");
+  document.querySelector("#dog1_container").classList.add("paused");
+  document.querySelector("#dog2_container").classList.add("paused");
+}
+// Funtion til at fjerne pause fra game elementer
+function dyrPlay() {
+  document.querySelector("#duck1_container").classList.remove("paused");
+  document.querySelector("#duck2_container").classList.remove("paused");
+  document.querySelector("#duck3_container").classList.remove("paused");
+  document.querySelector("#goose1_container").classList.remove("paused");
+  document.querySelector("#goose1_container").classList.remove("paused");
+  document.querySelector("#dog1_container").classList.remove("paused");
+  document.querySelector("#dog2_container").classList.remove("paused");
+}
+
 // ======================================== \\
 // ====== Clicking on a good element ====== \\
 // ======================================== \\
@@ -305,7 +338,7 @@ function duckClick() {
   // Laver lokal variabel
   let duck = this;
   // forhindre gentagne clicks
-  duck.removeEventListener("click", duckClick);
+  duck.removeEventListener("mousedown", duckClick);
 
   // stop duck container
   duck.classList.add("paused");
@@ -321,7 +354,7 @@ function duckClick() {
   document.querySelector("#sound_gunshot").volume = 0.05;
   document.querySelector("#sound_gunshot").play();
 
-  // Tilføjer +1 til points ved click
+  // Tilføjer +1 til points ved mousedown
   incrementDuckPoints();
 }
 
@@ -339,35 +372,43 @@ function duckGone() {
   //Fjerner pause fra container
   duck.classList.remove("paused");
 
-  // genstarter animationen for container
+  // genstarter alle elementer for duck
   duckRestart.call(this);
+
   // Tilføjer event så anden kan klikkes på igen
-  duck.addEventListener("click", duckClick);
+  duck.addEventListener("mousedown", duckClick);
 }
 
 function duckRestart() {
   console.log("duckRestart");
+  // genstarter animationen for container
+  duckPosition.call(this);
+
+  // sætter en ny bevægelse på container
+  duckMove.call(this);
+
+  // sætter en ny speed på container
+  speedClass.call(this);
+}
+
+function duckPosition() {
   // laver lokal variabel
   let duck = this;
 
   // Sætter nu position for container
   duck.classList.remove("positionDuck1", "positionDuck2", "positionDuck3");
-
   let pos = Math.floor(Math.random() * 3) + 1;
-
   duck.classList.add("positionDuck" + pos);
+}
+
+function duckMove() {
+  let duck = this;
 
   // Genstarter bevægelsen fra venstre mod højre
   duck.classList.remove("duck_move1", "duck_move2", "duck_move3");
-
+  duck.offsetWidth;
   let move = Math.floor(Math.random() * 3) + 1;
   duck.classList.add("duck_move" + move);
-
-  // sææter nu speed for container
-  duck.classList.remove("speed1", "speed2", "speed3", "speed4", "speed5");
-
-  let speed = Math.floor(Math.random() * 5) + 1;
-  duck.classList.add("speed" + speed);
 }
 
 // =========== Goose elements ============ \\
@@ -377,7 +418,7 @@ function gooseClick() {
   // Laver lokal variabel
   let goose = this;
   // forhindre gentagne clicks
-  goose.removeEventListener("click", gooseClick);
+  goose.removeEventListener("mousedown", gooseClick);
 
   // stop goose container
   goose.classList.add("paused");
@@ -393,7 +434,7 @@ function gooseClick() {
   document.querySelector("#sound_gunshot").volume = 0.05;
   document.querySelector("#sound_gunshot").play();
 
-  // Tilføjer +2 til points ved click
+  // Tilføjer +2 til points ved mousedown
   incrementGoosePoints();
 }
 
@@ -411,15 +452,36 @@ function gooseGone() {
   //Fjerner pause fra container
   goose.classList.remove("paused");
 
-  // genstarter animationen for container
+  // genstarter alle classer på goose element
   gooseRestart.call(this);
+
   // Tilføjer event så anden kan klikkes på igen
-  goose.addEventListener("click", gooseClick);
+  goose.addEventListener("mousedown", gooseClick);
 }
 
 function gooseRestart() {
   console.log("gooseRestart");
+  // genstarter animationen for container
+  goosePosition.call(this);
+
+  // sætter en ny bevægelse for container
+  gooseMove.call(this);
+
+  // sætter nu speed for container
+  speedClass.call(this);
+}
+
+function goosePosition() {
   // laver lokal variabel
+  let goose = this;
+
+  // Sætter nu position for container
+  goose.classList.remove("positionGoose1", "positionGoose2");
+  let pos = Math.floor(Math.random() * 2) + 1;
+  goose.classList.add("positionGoose" + pos);
+}
+
+function gooseMove() {
   let goose = this;
 
   //Genstarter bevægelse fra venstre mod højre
@@ -427,16 +489,15 @@ function gooseRestart() {
   goose.offsetWidth;
   let move = Math.floor(Math.random() * 2) + 1;
   goose.classList.add("goose_move" + move);
+}
 
-  // Sætter nu position for container
-  goose.classList.remove("positionGoose1", "positionGoose2");
-  let pos = Math.floor(Math.random() * 2) + 1;
-  goose.classList.add("positionGoose" + pos);
+// enkelt funktion for at tilføje et speed element til de 3 dyr
+function speedClass() {
+  let genstand = this;
 
-  // sææter nu speed for container
-  goose.classList.remove("speed1", "speed2", "speed3", "speed4", "speed5");
+  genstand.classList.remove("speed1", "speed2", "speed3", "speed4", "speed5");
   let speed = Math.floor(Math.random() * 5) + 1;
-  goose.classList.add("speed" + speed);
+  genstand.classList.add("speed" + speed);
 }
 
 // ======================================= \\
@@ -450,7 +511,7 @@ function dogClick() {
   // laver lokal variabel
   let dog = this;
   //Fjerner event så hun ikke kan klikkes på igen
-  dog.removeEventListener("click", dogClick);
+  dog.removeEventListener("mousedown", dogClick);
 
   // pauser move animationen for hund-container
   dog.classList.add("paused");
@@ -470,7 +531,7 @@ function dogClick() {
   document.querySelector("#sound_dogshot").volume = 0.1;
   document.querySelector("#sound_dogshot").play();
 
-  // fjern -1 fra lives ved click
+  // fjern -1 fra lives ved mousedown
   decrementLives();
 }
 
@@ -487,22 +548,27 @@ function dogGone() {
   //Fjerner pause fra container
   dog.classList.remove("paused");
 
-  //Genstarter bevægelse fra venstre mod højre
+  // genstarter alle classes for dog element
   dogRestart.call(this);
 
   // Tilføjer event så anden kan klikkes på igen
-  dog.addEventListener("click", dogClick);
+  dog.addEventListener("mousedown", dogClick);
 }
 
 function dogRestart() {
   console.log("dogRestart");
+  //Genstarter bevægelse fra venstre mod højre
+  dogPosition.call(this);
+
+  // sætter ny bevægelse på container
+  dogMove.call(this);
+
+  // sætter nu speed for container
+  speedClass.call(this);
+}
+function dogPosition() {
   // laver lokal variabel
   let dog = this;
-
-  //Genstarter bevægelse fra venstre mod højre
-  dog.classList.remove("dog_move1", "dog_move2");
-  let move = Math.floor(Math.random() * 2) + 1;
-  dog.classList.add("dog_move" + move);
 
   // Sætter nu position for container
   dog.classList.remove("positionDog1", "positionDog2");
@@ -510,11 +576,14 @@ function dogRestart() {
   let pos = Math.floor(Math.random() * 2) + 1;
 
   dog.classList.add("positionDog" + pos);
+}
 
-  // sææter nu speed for container
-  dog.classList.remove("speedDog1", "speedDog2");
+function dogMove() {
+  // laver lokal variabel
+  let dog = this;
 
-  let speed = Math.floor(Math.random() * 2) + 1;
-  dog.classList.add("speedDog" + speed);
-  console.log(`speed af ny dog er ${speed}`);
+  //Genstarter bevægelse fra venstre mod højre
+  dog.classList.remove("dog_move1", "dog_move2");
+  let move = Math.floor(Math.random() * 2) + 1;
+  dog.classList.add("dog_move" + move);
 }
